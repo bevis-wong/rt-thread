9f7a97d8ac5b260a7685221ebe90a616def2d0d4
bevis-wong <bevis@uliansz.com>
2019-03-10 09:49:30 +0800

    新增测试打印

--- bsp/stm32/stm32f767-st-nucleo/applications/main.c
+++ bsp/stm32/stm32f767-st-nucleo/applications/main.c
@@ -21,7 +21,7 @@ int main(void)
     int count = 1;
     /* set LED1 pin mode to output */
     rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
-
+    rt_kprintf("test git\r\n");
     while (count++)
     {
         rt_pin_write(LED1_PIN, PIN_HIGH);
